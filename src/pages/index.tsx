9255ffd4b7ac93ce54ae36ea8d52d2e54266d7be
Joshua Browne <joshuabrownenz@gmail.com>
2024-02-18 00:05:47 +1300

    Anchor Tags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,5 @@
 import * as React from "react";
 import { cn } from "../lib/utils";
-import {
-    TypographyH1,
-    TypographyH4,
-    TypographyP,
-} from "@/components/ui/typography";
 import { EarthAnimation } from "@/components/earthAnimation";
 import { LabelContainer } from "@/components/label/LabelContainer";
 import { Dialog, DialogContent } from "@/components/ui/dialog";
@@ -12,36 +7,53 @@ import { PAGE_DATA } from "@/constants";
 import { ProjectsDialogContent } from "@/components/projectsDialogContent/ProjectsDialogContent";
 import { Header } from "@/components/header/Header";
 import { MyStoryDialogContent } from "@/components/myStoryDialogContent/MyStoryDialogContent";
+import { PageProps, navigate } from "gatsby";
 
 type Section = "my-story" | "experience" | "projects";
+const isSection = (section: string): section is Section => ["my-story", "experience", "projects"].includes(section);
 
-export default function Index() {
-    const [dialogOpen, setDialogOpen] = React.useState(false);
-    const [activeTab, setActiveTab] = React.useState<Section | null>(null);
+export const Index: React.FC<PageProps> = ({ location }) => {
+    const hash = location.hash.slice(1);
+    const dialogOpen = isSection(hash);
 
-    const handleClick = (tab: Section) => {
-        setActiveTab(tab);
-        setDialogOpen(true);
+    const [activeTab, setActiveTab] = React.useState<Section | null>(dialogOpen ? hash : null);
+    console.log("activeTab", activeTab);
+    React.useEffect(() => {
+        if (isSection(hash)) {
+            setActiveTab(hash);
+        }
+    }, [hash]);
+
+    const handleOnOpenChange = (open: boolean) => {
+        if (!open) {
+            navigate("/");
+        }
     }
 
     return (
         <div className={cn("w-full min-h-screen md:max-h-screen md:overflow-hidden")}>
             <Header />
             <main className="aspect-[4/3] relative">
-                <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
+                <Dialog open={dialogOpen} onOpenChange={handleOnOpenChange}>
                     <DialogContent className="container">
                         {activeTab === "projects" && <ProjectsDialogContent projects={PAGE_DATA.projects} />}
                         {activeTab === "my-story" && <MyStoryDialogContent />}
                     </DialogContent>
                 </Dialog>
                 <div className="absolute z-50" style={{ left: "25%", top: "25%" }}>
-                    <LabelContainer angle="left45" text="Projects" onClick={() => handleClick("projects")} />
+                    <a href="/#projects">
+                        <LabelContainer angle="left45" text="Projects" />
+                    </a>
                 </div>
                 <div className="absolute z-50" style={{ left: "45%" }}>
-                    <LabelContainer angle="up" text="My Story" onClick={() => handleClick("my-story")} />
+                    <a href="/#my-story">
+                        <LabelContainer angle="up" text="My Story" />
+                    </a>
                 </div>
                 <div className="absolute z-50" style={{ top: "15%", right: "25%" }}>
-                    <LabelContainer angle="right45" text="Experience" onClick={() => handleClick("experience")} />
+                    <a href="/#experience">
+                        <LabelContainer angle="right45" text="Experience" />
+                    </a>
                 </div>
                 <EarthAnimation />
             </main>
@@ -49,3 +61,5 @@ export default function Index() {
         </div>
     );
 }
+
+export default Index;
